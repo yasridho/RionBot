1af1d056239d81b6a5aec7e62230aad84bdb9b39
Albatrous <42671837+Albatrous@users.noreply.github.com>
2018-11-17 17:15:15 +0700

    Test schedule

--- notif.py
+++ notif.py
@@ -8,7 +8,7 @@ from linebot.models import *
 def notifikasi():
 	line_bot_api.push_message('U3fed832cbef28b87b7827b306506c8d5', TextSendMessage(text="Yeeee"))
 
-schedule.every().day.at("17:13").do(notifikasi)
+schedule.every(10).seconds.do(notifikasi)
 
 while True:
 	schedule.run_pending()
