66f251f42e74efe3a4f263bf3d5f9d63922d1fe1
Albatrous <42671837+Albatrous@users.noreply.github.com>
2018-11-17 18:12:00 +0700

    Test schedule

--- notif.py
+++ notif.py
@@ -3,13 +3,22 @@ import os
 import schedule
 import time
 
+from datetime import datetime
+from threading import Timer
 from acc import (namaBot, google_key, line_bot_api, handler, db, notifikasi)
 from linebot.models import *
 
 Jalankan = False
 
+x = datetime.today()
+y = x.replace(second=x.second+10)
+
+secs = delta_t.seconds+1
+
 def yee():
 	line_bot_api.push_message('U3fed832cbef28b87b7827b306506c8d5', TextSendMessage(text="Yeeee"))
+
+t = Timer(secs, yee)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -19,11 +28,9 @@ def handle_message(event):
 	if text == "Run notif":
 		Jalankan = True
 		line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Notif berjalan"))
-		schedule.every(10).seconds.do(yee)
+		t.start()
 		
 	elif text == "Stop notif":
 		Jalankan = False
 		line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Notif berhenti"))
-
-while Jalankan:
-	schedule.run_continuously()
+		t.stop()
