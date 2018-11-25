b1d2279c2d29480f3f78d7df9d46376ff90ae4f9
Albatrous <42671837+Albatrous@users.noreply.github.com>
2018-11-25 15:07:53 +0700

    Test hasil permainan

--- quiz.py
+++ quiz.py
@@ -118,8 +118,7 @@ def handle_postback(event):
 					else:
 						line_bot_api.reply_message(event.reply_token, pemain(aturan))
 						playah.update({kirim:{"status":"pending","pemain":[]}})
-						durasi = 1*60 #1 menit
-						t = Timer(durasi, cek_pemain, (str(kirim)))
+						t = Timer(60, cek_pemain, [kirim])
 						t.start()
 				else:
 					line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Kabarin kalau udah siap ya kak ;D'))
