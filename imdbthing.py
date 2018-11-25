5002dc0ea13efd0d239440ca44820c6e74605249
Albatrous <42671837+Albatrous@users.noreply.github.com>
2018-11-25 21:06:30 +0700

    Menambahkan fitur imdb

--- imdbthing.py
+++ imdbthing.py
@@ -9,6 +9,7 @@ from linebot.models import *
 
 
 perintah = {}
+pencarian = {}
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
@@ -27,6 +28,47 @@ def handle_postback(event):
 				cmd, args = data[0].lower(), data[1]
 			else:
 				cmd, args = data[0].lower(), ""
+
+			if cmd == 'imdb':
+				cari, nomor = args.split(" ")
+				cari = cari.replace('+',' ')
+				batas = int(nomor)+9
+				hasil = pencarian[cari]
+				jhasil = len(hasil[cari]) - int(nomor)
+				res = list()
+				if jhasil > 10:
+					for i in hasil[int(nomor)+1:batas]:
+						res.append(i)
+					res.append(
+						BubbleContainer(
+							body=BoxComponent(
+								layout='vertical',
+								spacing='sm',
+								contents=[
+									ButtonComponent(
+										action=PostbackAction(
+											label='Selanjutnya',
+											text='Selain itu?',
+											data='/imdb '+cari.replace(' ','+')+' '+str(batas)
+										),
+										flex=1,
+										gravity='center'
+									)
+								]
+							)
+						)
+					)
+					hasil = res
+				else:
+					hasil = hasil[int(nomor)+1:]
+				
+				pesan = FlexSendMessage(
+						alt_text='Hasil pencarian: '+cari,
+						contents=CarouselContainer(
+							contents=hasil
+						)
+					)
+				line_bot_api.reply_message(event.reply_token, pesan)
 
 	except Exception as e:
 		try:
@@ -58,14 +100,50 @@ def handle_message(event):
 			komando, waktu = perintah[sender]
 
 			if komando == 'Cari film imdb':
-				hasil = search_movie_imdb(text)
-				pesan = FlexSendMessage(
-					alt_text='Hasil pencarian: '+text,
-					contents=CarouselContainer(
-						contents=hasil[:10]
+				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Sebentar kak '+panggil(sender)+', '+namaBot.capitalize()+' sedang menyiapkan hasil pencarian ;)'))
+				pencarian.update({text:[]})
+				for i in search_movie_imdb(text):
+					pencarian[text].append(i)
+				hasil = pencarian[text]
+				if len(hasil) > 10:
+					hasil = hasil[:9]
+					kirimin = list()
+					for r in hasil:
+						kirimin.append(r)
+					kirimin.append(
+						BubbleContainer(
+							body=BoxComponent(
+								layout='vertical',
+								spacing='sm',
+								contents=[
+									ButtonComponent(
+										action=PostbackAction(
+											label='Selanjutnya',
+											text='Selain itu?',
+											data='/imdb '+text.replace(' ','+')+' 9'
+										),
+										flex=1,
+										gravity='center'
+									)
+								]
+							)
+						)
 					)
-				)
-				line_bot_api.reply_message(event.reply_token, pesan)
+					for i in 
+					pesan = FlexSendMessage(
+						alt_text='Hasil pencarian: '+text,
+						contents=CarouselContainer(
+							contents=kirimin
+						)
+					)
+				else:
+					pesan = FlexSendMessage(
+						alt_text='Hasil pencarian: '+text,
+						contents=CarouselContainer(
+							contents=hasil
+						)
+					)
+				line_bot_api.push_message(sender, pesan)
 
 		if text == 'Cek film dong':
 			line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Kak '+panggil(sender)+' mau cari film apa?'))
